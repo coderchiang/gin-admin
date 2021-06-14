8422e90cebd358ca9731399d0b7b7c147e84e80d
chris <2501170033@qq.com>
2021-06-14 18:30:46 +0800

    feat: 2021-06-14 18:30:45 commit

--- web/src/settings/siteSetting.ts
+++ web/src/settings/siteSetting.ts
@@ -1,8 +1,8 @@
 // github repo url
-export const GITHUB_URL = 'https://github.com/anncwb/vue-vben-admin';
+export const GITHUB_URL = 'https://github.com/coderchiang/gin-admin';
 
 // vue-vben-admin-next-doc
-export const DOC_URL = 'https://vvbin.cn/doc-next/';
+export const DOC_URL = 'docusaurus-2-phi-ten.vercel.app';
 
 // site url
-export const SITE_URL = 'https://vvbin.cn/next/';
+export const SITE_URL = 'http://2wm.top';
